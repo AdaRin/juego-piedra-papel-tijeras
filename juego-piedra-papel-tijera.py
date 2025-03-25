9723cf0be93fff3aecc2a08739cc93d62759081a
Adriana <adry_ada@live.com.ar>
2025-03-25 09:05:00 -0300

    Modificamos las condiciones para usar Refactorización en el código

--- juego-piedra-papel-tijera.py
+++ juego-piedra-papel-tijera.py
@@ -3,12 +3,21 @@ nombre2 = input("Cómo se llamará el jugador 2?: ")
 
 print("Hola ", nombre1 + " eres el juegador número 1")
 jugador1 = input("Qué eliges? Piedra, papel o tijera?: ")
-print("Hola ", nombre2 + " eres el juegador número 1")
+print("Hola ", nombre2 + " eres el juegador número 2")
 jugador2 = input("Qué eliges? Piedra, papel o tijera?: ")
+
+
+#Separamos las condiciones realizando Refactorización
+
+condicion1 = jugador1=="piedra" and jugador2=="tijeras"
+condicion2 = jugador1=="papel" and jugador2=="pidra"
+condicion3 = jugador1=="tijeras" and jugador2=="papel"
+
 
 if jugador1 == jugador2:
     print("Ha sido un empate")
-elif (jugador1=="piedra" and jugador2=="tijeras") or (jugador1=="papel" and jugador2=="pidra") or (jugador1=="tijeras" and jugador2=="papel"):
+elif condicion1 or condicion2 or condicion3:
     print("Ha ganado el jugador ", nombre1)
 else:
         print("Ha ganado el jugador ", nombre2)
+
